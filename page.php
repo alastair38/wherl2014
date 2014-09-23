9cfe445383b0aa4ace565f34259097778e64283e
alastair38 <alastair@alastaircox.com>
2014-09-23 21:11:28 +0100

    Add home page loop
    Added pages loop to home page to display links to child pages on the
    home page

--- page.php
+++ page.php
@@ -24,28 +24,28 @@ Template Name: Home
 			
     				</div> <!-- end #main -->
     				
-                    <div id="" class="large-3 medium-4 small-12 columns" role="aside"><img src="http://placehold.it/308x215/8e3c2f/ffffff&text=Partners"></div>
-                    <div id="" class="large-3 medium-4 small-12 columns" role="aside"><img src="http://placehold.it/308x215/cdaf6a/ffffff&text=People"></div>
-                    <div id="" class="large-3 medium-4 small-12 columns" role="aside"><img src="http://placehold.it/308x215/66ccc5/ffffff&text=Findings"></div>
-                    <div id="" class="large-3 medium-4 small-12 columns" role="aside"><img src="http://placehold.it/308x215/4fc451/ffffff&text=News"></div>
-                    
-    
-				    
-				</div> <!-- end #inner-content -->
-				
-				<?php
-	$mypages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
+                   
+                	<?php
+	$mypages = get_pages( array( 'child_of' => $post->ID, 'exclude' => '20', 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
 
 	foreach( $mypages as $page ) {		
 		$content = $page->post_content;
 
 		$content = apply_filters( 'the_content', $content );
 	?>
-		<h2><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h2>
-		<div class="entry"><?php echo $content; ?></div>
+		<div class="large-3 columns">
+		<div class="home-links">
+		<h3><a href="<?php echo get_page_link( $page->ID ); ?>"><?php echo $page->post_title; ?></a></h3>
+		<?php echo $content; ?></div>
+		</div>
 	<?php
 	}	
-?>
+?>    
+    
+				    
+				</div> <!-- end #inner-content -->
+				
+			
     
 			</div> <!-- end #content -->
 
